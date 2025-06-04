c69bb6cac8157ab17f5b7f791a188314aeecfcfc
Kristiana <k.trupja@reply.de>
2025-06-04 18:05:36 +0200

    fix

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // No need for experimental.css
+  // Add other config here if needed
+};
+
+module.exports = nextConfig;
